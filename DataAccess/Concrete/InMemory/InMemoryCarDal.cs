78380b96f4598e1868ccd0fe238bf512a518639c
Onur ALTINTAŞ <onur40altintas@gmail.com>
2021-02-18 10:43:15 +0300

    Initial commit

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        private List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car
+                {
+                    Id = 1, ColorId = 1, BrandId = 1, ModelYear = "2014", DailyPrice = 50,
+                    Description = "Mazda 3 SkyActive"
+                },
+                new Car
+                {
+                    Id = 2, ColorId = 4, BrandId = 2, ModelYear = "1999", DailyPrice = 50,
+                    Description = "Opel Corsa"
+                },
+                new Car
+                {
+                    Id = 3, ColorId = 3, BrandId = 2, ModelYear = "2005", DailyPrice = 50,
+                    Description = "Fiat Punto"
+                },
+                new Car
+                {
+                    Id = 4, ColorId = 5, BrandId = 4, ModelYear = "2020", DailyPrice = 50,
+                    Description = "Ford Mondeo"
+                },
+                new Car
+                {
+                    Id = 5, ColorId = 2, BrandId = 3, ModelYear = "2008", DailyPrice = 50,
+                    Description = "Ford Kuga"
+                },
+            };
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.SingleOrDefault(c => c.Id == carId);
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            if (carToUpdate != null)
+            {
+                carToUpdate.BrandId = car.BrandId;
+                carToUpdate.ColorId = car.ColorId;
+                carToUpdate.DailyPrice = car.DailyPrice;
+                carToUpdate.Description = car.Description;
+                carToUpdate.ModelYear = car.ModelYear;
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+    }
+}
---
+++ ReCapProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ReCapProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            carManager.Add(new Car{Id = 6,BrandId = 4,ColorId = 3,DailyPrice = 25,ModelYear = "2011",Description = "Mazda 6 "});
+
+            carManager.Update(new Car{Id = 6,BrandId = 4,ColorId = 1,DailyPrice = 30,ModelYear="2011",Description = "Mazda 6 Skyactive"});
+
+            carManager.Delete(new Car{Id = 6});
+
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine(carManager.GetById(1).Description);
+
+            Console.WriteLine("-----------------------------");
+
+
+        }
+    }
+}
