59b289b3bbbd7d557e3cfa510419fc3c3df18411
Onur ALTINTAŞ <onur40altintas@gmail.com>
2021-03-08 12:07:06 +0300

    15. Hafta Ödev 1

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,7 @@
-﻿using Core.Utilities.IoC;
+﻿using System.Diagnostics;
+using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +12,10 @@ namespace Core.DependencyResolvers
         public void Load(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<HttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddMemoryCache();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<Stopwatch>();
         }
     }
 }
